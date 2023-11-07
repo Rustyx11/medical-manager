946c093f1fdd62050bdec6b6e5aff1d4845df2d8
Rustyx11 <97688988+Rustyx11@users.noreply.github.com>
2023-11-07 20:13:28 +0100

    Wersja 07.11 20:10

---
+++ models/dModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const doctorSchema = new mongoose.Schema(
+  {
+    Iduser: {
+      type: String,
+    },
+    Name: {
+      type: String,
+      require: [true, "Imie jest wymagane"],
+    },
+    LastName: {
+      type: String,
+      require: [true, "Nazwisko jest wymagane"],
+    },
+    Phone: {
+      type: String,
+      require: [true, "Numer telefonu jest wymagany"],
+    },
+    Email: {
+      type: String,
+      require: [true, "Adres email jest wymagany"],
+    },
+    Address: {
+      type: String,
+      require: [true, "Adres zamieszkania jest wymagany"],
+    },
+    Specialization: {
+      type: String,
+      require: [true, "Specjalizacja lekarza jest wymagana."],
+    },
+    Time: {
+      type: Object,
+      require: [true, "Czas pracy lekarza."],
+    },
+  },
+  { timestamps: true }
+);
+
+const doctorModel = mongoose.model("users", doctorSchema);
+module.exports = doctorModel;
